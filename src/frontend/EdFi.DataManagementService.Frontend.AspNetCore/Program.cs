71866242297f90528d347e22eae450023875e66d
Max Paulson <max_paulson@dpsk12.net>
2024-08-29 11:06:57 -0600

    placing lambda tools on the ASP Net Frontend

--- src/frontend/EdFi.DataManagementService.Frontend.AspNetCore/Program.cs
+++ src/frontend/EdFi.DataManagementService.Frontend.AspNetCore/Program.cs
@@ -16,6 +16,10 @@ Environment.SetEnvironmentVariable("DOTNET_hostBuilder:reloadConfigOnChange", "f
 var builder = WebApplication.CreateBuilder(args);
 builder.AddServices();
 
+// Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
+// package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+
 var app = builder.Build();
 
 app.UseMiddleware<LoggingMiddleware>();
